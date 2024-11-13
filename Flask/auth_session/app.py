1c4b20cc8a0d83b2a801c3c7cc79b00039540d15
DevMon724 <devmon724@gmail.com>
2024-11-13 16:55:50 +0900

    flask 5th challenge

---
+++ Flask/auth_session/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, redirect, session, flash
+from datetime import timedelta
+app = Flask(__name__)
+
+app.secret_key = 'flask-secret-key' # 실제로 배포시에는 .env or yaml 에 넣어서 배포
+app.config['PERMANENT_SESSEION_LIFETIME'] = timedelta(days=7)
+
+users = {
+    'john' : 'pw123',
+    'leo' : 'pw123'
+}
+
+@app.route('/')
+def index():
+    return render_template("login.html")
+
+@app.route('/login', methods = ["POST"])
+def login() :
+    username = request.form["username"]
+    password = request.form["password"]
+
+    if username in users and users[username] == password:
+        session['username'] = username
+        session.permanent = True
+
+        return redirect('/secret')
+    else :
+        flash("Invalid username or password")
+        return redirect('/')
+    
+@app.route('/secret')
+def secret() :
+    if "username" in session :
+        return render_template("secret.html")
+    else :
+        return redirect('/')
+    
+#로그 아웃
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
